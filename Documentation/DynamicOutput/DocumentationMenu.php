aecd0f1fd503618b1b6e8f0a663ecc282dd3338d
sevidmusic <sdmwebsdm@gmail.com>
2021-07-31 03:14:05 -0400

    Resolved issue #1 and issue #2, the Documentation and DocumentationDarkTheme App packages have been migrated from the ddmsAppPackages repo.

---
+++ Documentation/DynamicOutput/DocumentationMenu.php
@@ -0,0 +1,21 @@
+<div class="documentation-output-container documentation-sticky-top">
+    <div class="documentation-output-menu-container">
+        <ul>
+            <li>
+                <a href="index.php?request=AboutTheRoady">
+                    About
+                </a>
+            </li>
+            <li>
+                <a href="index.php?request=GettingStarted">
+                    Getting Started
+                </a>
+            </li>
+            <li>
+                <a href="index.php?request=Installation">
+                    Installation
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
