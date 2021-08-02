56f8bcd09c9594e47ba0e4a604a6c402b4a65587
sevidmusic <sdmwebsdm@gmail.com>
2021-08-02 16:41:17 -0400

    Continued addressing issue #7. Began definiing regex patterns used to format plain text help files defined by rig into html formatted output.

--- RoadyAndRigDocs/DynamicOutput/Documentation.php
+++ RoadyAndRigDocs/DynamicOutput/Documentation.php
@@ -24,30 +24,40 @@ $helpFilesListing = array_diff(
     ['.', '..']
 );
 
-foreach($helpFilesListing as $listing) {
-    echo '<div><a href="?request=' . str_replace('.txt', '', $listing) . '">' . $listing . '</a></div>';
-}
-
-if(empty($currentRequest->getGet())) {
-    $output = strval(file_get_contents($rigHelpFilesDirectoryPath . DIRECTORY_SEPARATOR . 'help.txt'));
-}
-
-if(!empty($currentRequest->getGet())) {
-    $output = strval(file_get_contents($rigHelpFilesDirectoryPath . DIRECTORY_SEPARATOR . ($currentRequest->getGet()['request'] ?? 'help') . '.txt'));
-}
+$output = htmlspecialchars(strval(file_get_contents($rigHelpFilesDirectoryPath . DIRECTORY_SEPARATOR . ($currentRequest->getGet()['request'] ?? 'help') . '.txt')));
 
 ?>
 
-<div class="roady-and-rig-docs-output-container">
+<div class="rr-docs-container">
 
 <?php
 
+    $codeClass = 'rr-docs-code';
     $lines = explode(PHP_EOL, $output);
     foreach($lines as $key => $line) {
         $lines[$key] = '<p>' . trim($line) . '</p>';
     }
     $output = str_replace('<p></p>', '',implode(PHP_EOL, $lines));
-    echo $output;
+    foreach($helpFilesListing as $listing) {
+        echo '<div><a href="?request=' . str_replace('.txt', '', $listing) . '">' . $listing . '</a></div>';
+    }
+    echo preg_replace(
+        [
+            '/rig --.*</m',
+            '/\[.*]/m',
+            '/rig.*\[/',
+#            '#((\.\/)\w+/).*(\w+/)(\w+.*[.]\w+)#',
+            '#((\.\/)\w+/).*(\w+/)(\w+.*[.]\w+)|((\/)\w+/).*(\w+/)(\w+)#',
+        ],
+        [
+            '<code class="' . $codeClass . '">${0}/code><',
+            '<code class="' . $codeClass . '">${0}</code>',
+            '<code class="' . $codeClass . '">rig [',
+#            '<code class="rr-docs-file-path">${0}</code>',
+            '<code class="rr-docs-file-path">${0}</code>',
+        ],
+        $output
+    );
 ?>
 
 </div>
--- RoadyAndRigDocs/make.sh
+++ RoadyAndRigDocs/make.sh
@@ -6,7 +6,7 @@ set -o posix
 
 rig --new-app --name RoadyAndRigDocs --domain 'http://localhost:8080'
 
-rig --new-global-response --for-app 'RoadyAndRigDocs' --name 'Documentation' --position '0'
+rig --new-global-response --for-app 'RoadyAndRigDocs' --name 'Documentation' --position '2'
 
 rig --new-request --for-app 'RoadyAndRigDocs' --name 'Documentation' --relative-url 'index.php?request=Documentation' --container 'RoadyAndRigDocsRequests'
 
