642f4dd385265fe558009474191b9266f5a9a602
sevidmusic <sdmwebsdm@gmail.com>
2021-08-03 11:21:43 -0400

    Housekeeping, working on issue #7

--- RoadyAndRigDocs/DynamicOutput/Documentation.php
+++ RoadyAndRigDocs/DynamicOutput/Documentation.php
@@ -54,32 +54,50 @@ $output = htmlspecialchars(
             foreach($helpFilesListing as $listing) {
                 echo '<div><a href="?request=' . str_replace('.txt', '', $listing) . '">' . $listing . '</a></div>';
             }
+
             echo preg_replace(
                 [
                     # Match similar to [--flag] [--flag] [--flag]
-                    '/\[.*]/m',
+                    '/\[.*]/',
                     # Match paths (./path/.../word OR /path/.../word)
                     '#((\.\/)\w+/).*(\w+/)(\w+.*[.]\w+)|((\/)\w+/).*(\w+/)(\w+)|((\.\/)\w+/).*(\w+/)(\w+)#',
-                    # Match --flag... word \
-                    '/((--)\w+).*[ ](\w+)[ ][\\\]/m',
-                    # Match --flag...<
-                    '/rig --.*</m',
+                    # Match --word... word \
+                    '/((--)\w+).*[ ](\w+)[ ][\\\]/',
+                    # Match rig --word...<
+                    '/rig --.*</',
+                    # Match --word... word<
+                    '/((--)\w+).*[ ](\w+)</',
                     # Match export PATH=&quot;${PATH}:${HOME}<code class="rr-docs-file-path">
                     '/export PATH=&quot;\${PATH}:\${HOME}<code class="rr-docs-file-path">/',
+                    # Match bin</code>&quot;
                     '/bin<\/code>&quot;/',
+                    # Match ~/word
                     '/~\/(\w+)/',
+                    # Match $PATH (with one space on each side)
                     '/[ ]\$PATH[ ]/',
+                    # Match rig  <code class="rr-docs-code">
                     '/rig  <code class="rr-docs-code">/',
                 ],
                 [
+                    # Match similar to [--flag] [--flag] [--flag]
                     '<code class="' . $codeClass . '">${0}</code>',
+                    # Match paths (./path/.../word OR /path/.../word)
                     '<code class="' . $pathClass . '">${0}</code>',
+                    # Match --flag... word \
                     '<code class="' . $codeClass . '">${0}</code>',
+                    # Match --word... word<
                     '<code class="' . $codeClass . '">${0}/code><',
+                    # Match rig --flag ...word<
+                    '<code class="' . $codeClass . '">${0}/code><',
+                    # Match export PATH=&quot;${PATH}:${HOME}<code class="rr-docs-file-path">
                     '<code class="' . $codeClass . '">export PATH=&quot;${PATH}:${HOME}',
+                    # Match bin</code>&quot;
                     'bin&quot;</code>',
+                    # Match ~/word
                     '<code class="' . $pathClass . '">${0}</code>',
+                    # Match $PATH (with one space on each side)
                     '<code class="' . $codeClass . '">$PATH</code>',
+                    # Match rig  <code class="rr-docs-code">
                     '<code class="rr-docs-code">rig ',
                 ],
                 $output
