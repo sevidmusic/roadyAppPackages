ead58ef46857b2d93c72668e90f29a8a0c7b181e
sevidmusic <sdmwebsdm@gmail.com>
2021-08-03 00:48:31 -0400

    Working on addressing issue #7, modified regex patterns for output filtering.

--- RoadyAndRigDocs/DynamicOutput/Documentation.php
+++ RoadyAndRigDocs/DynamicOutput/Documentation.php
@@ -17,45 +17,57 @@ $roadyRootDirectoryPath = str_replace(
     __DIR__
 );
 
-$rigHelpFilesDirectoryPath = strval(realpath($roadyRootDirectoryPath . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'darling' . DIRECTORY_SEPARATOR . 'rig' . DIRECTORY_SEPARATOR . 'helpFiles'));
+$rigHelpFilesDirectoryPath = strval(
+    realpath(
+        $roadyRootDirectoryPath . DIRECTORY_SEPARATOR . 'vendor' .
+        DIRECTORY_SEPARATOR . 'darling' . DIRECTORY_SEPARATOR . 'rig' .
+        DIRECTORY_SEPARATOR . 'helpFiles'
+    )
+);
 
 $helpFilesListing = array_diff(
     scandir($rigHelpFilesDirectoryPath),
     ['.', '..']
 );
 
-$output = htmlspecialchars(strval(file_get_contents($rigHelpFilesDirectoryPath . DIRECTORY_SEPARATOR . ($currentRequest->getGet()['request'] ?? 'help') . '.txt')));
+$output = htmlspecialchars(
+    strval(
+        file_get_contents(
+            $rigHelpFilesDirectoryPath . DIRECTORY_SEPARATOR .
+            ($currentRequest->getGet()['request'] ?? 'help') . '.txt'
+        )
+    )
+);
 
 ?>
 
 <div class="rr-docs-container">
-
-<?php
-
-    $codeClass = 'rr-docs-code';
-    $lines = explode(PHP_EOL, $output);
-    foreach($lines as $key => $line) {
-        $lines[$key] = '<p>' . trim($line) . '</p>';
-    }
-    $output = str_replace('<p></p>', '',implode(PHP_EOL, $lines));
-    foreach($helpFilesListing as $listing) {
-        echo '<div><a href="?request=' . str_replace('.txt', '', $listing) . '">' . $listing . '</a></div>';
-    }
-    echo preg_replace(
-        [
-            '/rig --.*</m',
-            '/\[.*]/m',
-            '/rig.*\[/',
-            '#((\.\/)\w+/).*(\w+/)(\w+.*[.]\w+)|((\/)\w+/).*(\w+/)(\w+)|((\.\/)\w+/).*(\w+/)(\w+)#',
-        ],
-        [
-            '<code class="' . $codeClass . '">${0}/code><',
-            '<code class="' . $codeClass . '">${0}</code>',
-            '<code class="' . $codeClass . '">rig [',
-            '<code class="rr-docs-file-path">${0}</code>',
-        ],
-        $output
-    );
-?>
-
+    <div class="rr-docs-output">
+        <?php
+            $codeClass = 'rr-docs-code';
+            $lines = explode(PHP_EOL, $output);
+            foreach($lines as $key => $line) {
+                $lines[$key] = '<p>' . trim($line) . '</p>';
+            }
+            $output = str_replace('<p></p>', '',implode(PHP_EOL, $lines));
+            foreach($helpFilesListing as $listing) {
+                echo '<div><a href="?request=' . str_replace('.txt', '', $listing) . '">' . $listing . '</a></div>';
+            }
+            echo preg_replace(
+                [
+                    '/\[.*]/m', # Match similar to [--flag] [--flag] [--flag]
+                    '#((\.\/)\w+/).*(\w+/)(\w+.*[.]\w+)|((\/)\w+/).*(\w+/)(\w+)|((\.\/)\w+/).*(\w+/)(\w+)#',
+                    '/((--)\w+).*[ ](\w+)[ ][\\\]/m',
+                    '/rig --.*</m',
+                ],
+                [
+                    '<code class="' . $codeClass . '">${0}</code>',
+                    '<code class="rr-docs-file-path">${0}</code>',
+                    '<code class="' . $codeClass . '">${0}</code>',
+                    '<code class="' . $codeClass . '">${0}/code><',
+                ],
+                $output
+            );
+        ?>
+    </div>
 </div>
