59487dc8fec82de024ec5e8ee190a237b7d74388
sevidmusic <sdmwebsdm@gmail.com>
2021-11-17 12:16:37 -0500

    Updated the HelloWorld App to reflect the HelloWorld App created in the example from the `rig --help AppPackages` documentation.

--- HelloWorld/make.sh
+++ HelloWorld/make.sh
@@ -10,12 +10,16 @@ rig --new-response --for-app 'HelloWorld' --name 'HelloWorld' --position '0'
 
 rig --new-request --for-app 'HelloWorld' --name 'HelloWorld' --relative-url 'index.php?request=HelloWorld' --container 'HelloWorldRequests'
 
-rig --new-request --for-app 'HelloWorld' --name 'HelloWorld0' --relative-url '' --container 'HelloWorldRequests'
+rig --new-request --for-app 'HelloWorld' --name 'HelloWorld0' --relative-url 'index.php' --container 'HelloWorldRequests'
+
+rig --new-request --for-app 'HelloWorld' --name 'HelloWorld1' --relative-url '' --container 'HelloWorldRequests'
 
 rig --new-dynamic-output-component --for-app 'HelloWorld' --name 'HelloWorld' --container 'HelloWorldDynamicOutput' --position '0' 
 
+rig --assign-to-response --for-app 'HelloWorld' --response 'HelloWorld' --requests 'HelloWorld' 
+
 rig --assign-to-response --for-app 'HelloWorld' --response 'HelloWorld' --requests 'HelloWorld0' 
 
-rig --assign-to-response --for-app 'HelloWorld' --response 'HelloWorld' --requests 'HelloWorld' 
+rig --assign-to-response --for-app 'HelloWorld' --response 'HelloWorld' --requests 'HelloWorld1' 
 
 rig --assign-to-response --for-app 'HelloWorld' --response 'HelloWorld' --dynamic-output-components 'HelloWorld' 
