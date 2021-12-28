09f6a066c08cf5a3ca08d4d9708de15a350e23a5
sevidmusic <sevidmusic@gmail.com>
2021-12-27 20:29:00 -0500

    Working on RoadyMediaplayer's Video class.
    Implemented the following tests in
    RoadyMediaPlayer/resources/tests/component/media/VideoTest.php:
    
    testMediaIsAccessibleReturnsFalseIfMediasUrlPointsToMediaWithAnUnSupportedMimeType(): void
    testMediaIsAccessibleReturnsTrueIfMediasUrlPointsToMediaWithASupportedMimeType(): void
    testMediaIsAccessibleReturnsTrueIfRequestToMediasUrlReturnsHttpResponseCode200(): void
    
    All tests are passing.

--- RoadyMediaPlayer/resources/classes/component/media/Video.php
+++ RoadyMediaPlayer/resources/classes/component/media/Video.php
@@ -2,6 +2,48 @@
 
 namespace Apps\RoadyMediaPlayer\resources\classes\component\media;
 
+use \Exception;
+
 class Video extends Media {
+
+    /**
+     * @return array<int, string> An array of Mime types supported 
+     *                            video Mime Types.
+     */
+    private function supportedVideoMimeTypes(): array 
+    {
+        return [
+            'video/webm',
+            'video/mp4',
+        ];
+    }
+
+    public function mediaIsAccessible(): bool
+    {
+        try {
+            $headers = get_headers(
+                $this->mediaUrl(), 
+                associative: true
+            );
+            $contentType = (
+                is_array($headers) 
+                ? ($headers['Content-Type'] ?? 'COULD_NOT_DETERMINE_MIME_TYPE') 
+                : 'FAILED_TO_GET_HEADER'
+            );
+            if(in_array($contentType, $this->supportedVideoMimeTypes())) {
+                return parent::mediaIsAccessible(); 
+            }
+        } catch (Exception $e) {
+            $this->log(
+                'Failed to determine if media is accessible, an error occurred: %s',
+                $e->getMessage()
+            );
+        }
+        $this->log(
+            'The requested media is not a supported Video type, media url: %s',
+            $this->mediaUrl()
+        );
+        return false; 
+    }
 
 }
--- RoadyMediaPlayer/resources/tests/component/media/VideoTest.php
+++ RoadyMediaPlayer/resources/tests/component/media/VideoTest.php
@@ -51,4 +51,52 @@ class VideoTest extends MediaTest
             $metaData
         );
     }
+
+    /**
+     * @return array<int, string> An array of Mime types supported 
+     *                            video Mime Types.
+     */
+    private function supportedVideoMimeTypes(): array 
+    {
+        return [
+            'video/webm',
+            'video/mp4',
+        ];
+    }
+
+    public function testMediaIsAccessibleReturnsFalseIfMediasUrlPointsToMediaWithAnUnSupportedMimeType(): void
+    {
+        $video = $this->newVideoInstance(mediaUrl: 'https://sevidmusic.us-east-1.linodeobjects.com/Lies_by_SeviD_20210902.mp3');
+        $this->assertFalse(
+            $video->mediaIsAccessible()
+        );
+    }
+
+    public function testMediaIsAccessibleReturnsTrueIfMediasUrlPointsToMediaWithASupportedMimeType(): void
+    {
+        $validMediaUrls = [
+            'https://roadydemos.us-east-1.linodeobjects.com/QuickInstallSetupHelloWorldFinal.webm',
+            'https://sevidmusic.us-east-1.linodeobjects.com/Lies_by_SeviD_20210902.mp4',
+        ];
+        foreach($validMediaUrls as $mediaUrl) {
+            $video = $this->newVideoInstance(mediaUrl: $mediaUrl);
+            $this->assertTrue(
+                $video->mediaIsAccessible()
+            );
+        }
+    }
+
+    public function testMediaIsAccessibleReturnsTrueIfRequestToMediasUrlReturnsHttpResponseCode200(): void
+    {
+        /** 
+         * Overwrite parent test method:
+         *
+         *  MediaTest::testMediaIsAccessibleReturnsTrueIfRequestToMediasUrlReturnsHttpResponseCode200(), 
+         *
+         * A 200 response is not sufficient, Mime/Content Type must 
+         * match as well. 
+         *
+         */
+        $this->testMediaIsAccessibleReturnsTrueIfMediasUrlPointsToMediaWithASupportedMimeType();
+    }
 }
