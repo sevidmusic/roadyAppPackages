d8d24ce1af97f578441f96578a5613bb3046e921
sevidmusic <sevidmusic@gmail.com>
2022-02-24 22:54:21 -0500

    Working on new Apps: roadyDarkTheme, and roadyThemeTester

---
+++ roadyThemeTester/DynamicOutput/StylePreview.php
@@ -0,0 +1,57 @@
+<div class="roady-app-output-contianer">
+    <nav class="roady-navigation-menu">
+        <a href="index.php">&lt;nav&gt;</a>
+        <a href="https://roady.tech" target="_blank" rel="noopener noreferrer">roady.tech</a>
+        <a href="https://github.com/sevidmusic/roady" target="_blank" rel="noopener noreferrer">roady on GitHub</a>
+    </nav>
+    <ul class="roady-navigation-list">
+        <li><a href="index.php">&lt;nav&gt;</a></li>
+        <li><a href="https://roady.tech" target="_blank" rel="noopener noreferrer">roady.tech</a></li>
+        <li><a href="https://github.com/sevidmusic/roady" target="_blank" rel="noopener noreferrer">roady on GitHub</a></li>
+    </ul>
+    <ul class="roady-navigation-bar">
+        <li><a href="index.php">&lt;nav&gt;</a></li>
+        <li><a href="https://roady.tech" target="_blank" rel="noopener noreferrer">roady.tech</a></li>
+        <li><a href="https://github.com/sevidmusic/roady" target="_blank" rel="noopener noreferrer">roady on GitHub</a></li>
+    </ul>
+    <ul class="roady-ul-list">
+        <li>Unordered List item</li>
+        <li>Unordered List item</li>
+        <li>Unordered List item</li>
+        <li>Unordered List item</li>
+        <li>Unordered List item</li>
+    </ul>
+    <ol class="roady-ol-list">
+        <li>Ordered List item</li>
+        <li>Ordered List item</li>
+        <li>Ordered List item</li>
+        <li>Ordered List item</li>
+        <li>Ordered List item</li>
+    </ol>
+    <h1>A h1 header</h1>
+    <h2>A h1 header</h1>
+    <h3>A h1 header</h1>
+    <h4>A h1 header</h1>
+    <h5>A h1 header</h1>
+    <h6>A h1 header</h1>
+    <p>A paragraph, lorem ipsum, Puella in piscinam cecidit.</p>
+    <pre>
+Text within a &lt;pre&gt; tag.
+    </pre>
+    <p>
+        An inline code snippet: <code class="roady-inline-code">$inline = 'code snippet';</code>
+        Lorem ipsum, amo amor amas.
+    </p>
+    <pre class="roady-multi-line-code-container">
+        <code class="roady-multi-line-code">
+// multi line code snippet within a &lt;pre&gt; tag
+&lt;?php echo 'text within a &lt;pre&gt; tag'; ?&gt;
+        </code>
+    </pre>
+<div class="roady-app-output-contianer-grid">
+    <div class="roady-app-output-contianer-grid-item"><img src="roadyLogo.png"></div>
+    <div class="roady-app-output-contianer-grid-item"><img src="roadyLogo.png"></div>
+    <div class="roady-app-output-contianer-grid-item"><img src="roadyLogo.png"></div>
+    <div class="roady-app-output-contianer-grid-item"><img src="roadyLogo.png"></div>
+</div>
+</div>
