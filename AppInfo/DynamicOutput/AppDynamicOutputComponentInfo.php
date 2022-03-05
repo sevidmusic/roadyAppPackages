2dd3d7a122f48ef0d0d2262a057170e52af6b0af
sevidmusic <sevidmusic@gmail.com>
2022-03-05 15:00:09 -0500

    Working on cleaning up AppInfo App's source code

--- AppInfo/DynamicOutput/AppDynamicOutputComponentInfo.php
+++ AppInfo/DynamicOutput/AppDynamicOutputComponentInfo.php
@@ -3,17 +3,9 @@
 use Apps\AppInfo\resources\config\Sprints;
 use Apps\AppInfo\resources\config\CoreComponents;
 use Apps\AppInfo\resources\config\ComponentInfo;
-
-$appsConfiguredComponentOverviewHtml = ComponentInfo::appsConfiguredComponentOverviewHtml();
+use roady\classes\component\DynamicOutputComponent;
 
 printf(
     Sprints::outputContainerSprint(),
-    (
-    empty($appsConfiguredComponentOverviewHtml)
-    ? ComponentInfo::noConfiguredComponentsMessage(
-        'DynamicOutputComponent',
-        'new-dynamic-output-component'
-    ) 
-    : $appsConfiguredComponentOverviewHtml
-    )
+    ComponentInfo::htmlOverviewOfAppsConfiguredComponents(DynamicOutputComponent::class)
 );
--- AppInfo/resources/config/ComponentInfo.php
+++ AppInfo/resources/config/ComponentInfo.php
@@ -6,16 +6,103 @@ use roady\interfaces\component\Component;
 use Apps\AppInfo\resources\config\CoreComponents;
 use Apps\AppInfo\resources\config\Sprints;
 use roady\classes\component\DynamicOutputComponent;
+use roady\classes\component\OutputComponent;
 
+/**
+ *
+ * Methods:
+ * public static function htmlOverviewOfAppsConfiguredComponents(string $componentType): string
+ * public static function noConfiguredComponentsMessage(
+ *     string $componentType, 
+ *     string $commandHint
+ * ): string
+ */
 class ComponentInfo 
 {
-    public static function componentInfoHtml(Component $component): string {
+
+    /**
+     * Return html formatted overview of the currently requested App's
+     * configured Components. Only the Components whose type matches
+     * the specified type will be included in the overview.
+     *
+     * If there aren't any Components of the specified type configured
+     * by the requested App, then an empty string will be returned.
+     *
+     * @example:
+     *     ComponentInfo::htmlOverviewOfAppsConfiguredComponents(DynamicOutputComponent::class);
+     *
+     * @param class-string $componentType The type of Component to 
+     *                                    include in the overview.
+     */
+    public static function htmlOverviewOfAppsConfiguredComponents(string $componentType): string 
+    {
+        $generateHtmlOverviewOfAppsConfiguredComponents = self::generateHtmlOverviewOfAppsConfiguredComponents($componentType);
+        return (
+            empty($generateHtmlOverviewOfAppsConfiguredComponents) 
+            ? ComponentInfo::noConfiguredComponentsMessage($componentType)
+            : implode(PHP_EOL, $generateHtmlOverviewOfAppsConfiguredComponents)
+        );
+    }
+
+    /**
+     * @param class-string $componentType The type of Component to 
+     *                                    include in the overview.
+     * @return array<int, string>
+     */
+    private static function generateHtmlOverviewOfAppsConfiguredComponents(string $componentType): array 
+    {
+        $htmlOverviewOfAppsConfiguredComponents = [];
+        foreach (
+            CoreComponents::appsAppComponentsFactory()
+                ->getStoredComponentRegistry()
+                ->getRegisteredComponents()
+            as
+            $registeredComponent
+        ) {
+            if($registeredComponent::class === $componentType) {
+                array_push(
+                    $htmlOverviewOfAppsConfiguredComponents,
+                    self::componentInfoHtml($registeredComponent)
+                );
+            }
+        }
+        return $htmlOverviewOfAppsConfiguredComponents;
+    }
+
+    public static function noConfiguredComponentsMessage(
+        string $componentType, 
+    ): string
+    {
+        $appName = (
+            CoreComponents::currentRequest()->getGet()['appName'] 
+            ?? 
+            'roady'
+        );
+        return "
+        <p class='roady-message'>
+            There are no $componentType's configured for the 
+            $appName app.
+        </p>
+        <p class=\"roady-note\">
+            Hint: <a href=\"https://roady.tech/index.php?request=rig\">rig</a>
+            can be used to configure various types of Components for
+            an App.
+        </a>
+        ";
+    }
+
+    private static function componentInfoHtml(Component $component): string {
         switch($component->getType() ) {
             case DynamicOutputComponent::class:
                 /**
                  * @var DynamicOutputComponent $component
                  */
                 return self::dynamicOutputComponentInfo($component);
+            case OutputComponent::class:
+                /**
+                 * @var OutputComponent $component
+                 */
+                return self::outputComponentInfo($component);
             default: return '';
         }
     }
@@ -54,49 +141,9 @@ class ComponentInfo
         );
     }
 
-    public static function appsConfiguredComponentOverviewHtml(): string 
+    private static function outputComponentInfo(OutputComponent $component): string
     {
-        $appsConfiguredComponentOverviewHtml = [];
-        foreach (
-            CoreComponents::appsAppComponentsFactory()
-                ->getStoredComponentRegistry()
-                ->getRegisteredComponents()
-            as
-            $registeredComponent
-        ) {
-            array_push(
-                $appsConfiguredComponentOverviewHtml,
-                self::componentInfoHtml($registeredComponent)
-            );
-        }
-        return (
-            empty($appsConfiguredComponentOverviewHtml) 
-            ? '' 
-            : implode(PHP_EOL, $appsConfiguredComponentOverviewHtml)
-        );
-    }
-
-    public static function noConfiguredComponentsMessage(
-        string $componentType, 
-        string $commandHint
-    ): string
-    {
-        $appName = (
-            CoreComponents::currentRequest()->getGet()['appName'] 
-            ?? 
-            'roady'
-        );
-        return "
-        <p class='roady-message'>
-            There are no $componentType's configured for the 
-            $appName app.
-        </p>
-        <p class='roady-note'>To configure a new $componentType use 
-        <code class='roady-inline-code'>
-        <a href=\"" . Sprints::onlineDocumentationRequestSprint() . 
-        $commandHint . "\" target='_blank' rel='noopener noreferrer'>
-            rig --$commandHint
-        </a></code></p>";
+        return 'OC OC OC';
     }
 
 }
