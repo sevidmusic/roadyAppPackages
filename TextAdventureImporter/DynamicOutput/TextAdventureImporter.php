69b9b361a6f2c6c08a67b0949645868519eaf5a5
sevidmusic <sevidmusic@gmail.com>
2022-04-22 13:38:14 -0400

    Working on TextAdventureImporter, added roady css classes to appropriate html elements. The uploads directory is now created if it does not exist on upload

--- TextAdventureImporter/DynamicOutput/TextAdventureImporter.php
+++ TextAdventureImporter/DynamicOutput/TextAdventureImporter.php
@@ -19,26 +19,29 @@ if(isset($_POST["submit"])) {
       echo "Sorry, file already exists.";
       $uploadOk = 0;
     }
-    
+
     // Check file size
     if (($_FILES["fileToUpload"]["size"] ?? 5000000) > 5000000) {
       echo "Sorry, your file is too large.";
       $uploadOk = 0;
     }
-    
+
     // Allow certain file formats
     if($uploadedFileType != "html") {
       echo "Sorry, only html files are allowed.";
       $uploadOk = 0;
     }
-    
+
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
       echo "Sorry, your file was not uploaded.";
     // if everything is ok, try to upload file
-    } 
-    
+    }
+
     else {
+        if(!is_dir($target_dir)) {
+            mkdir($target_dir);
+        }
       if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
       } else {
@@ -48,8 +51,8 @@ if(isset($_POST["submit"])) {
 }
 ?>
 
-<form action="index.php?request=TextAdventureImporter" method="post" enctype="multipart/form-data">
-  Select image to upload:
-  <input type="file" name="fileToUpload" id="fileToUpload">
-  <input type="submit" value="Import Twine File" name="submit">
+<form class="roady-form" action="index.php?request=TextAdventureImporter" method="post" enctype="multipart/form-data">
+  <label class="roady-form-input-label" for="fileToUpload">Select A Twine Html File To Upload:</label>
+  <input id="fileToUpload" class="roady-form-input" type="file" name="fileToUpload">
+  <input type="submit" class="roady-form-input" value="Import Twine File" name="submit">
 </form>
