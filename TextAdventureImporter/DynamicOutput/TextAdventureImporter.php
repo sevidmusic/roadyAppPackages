49f352bdda58b99fc87f2a10a4a78b932937af9a
sevidmusic <sevidmusic@gmail.com>
2022-04-22 14:27:02 -0400

    Working on TextAdventureImporter

--- TextAdventureImporter/DynamicOutput/TextAdventureImporter.php
+++ TextAdventureImporter/DynamicOutput/TextAdventureImporter.php
@@ -12,41 +12,39 @@ $fileToUpload = ($_FILES["fileToUpload"]["tmp_name"] ?? 'notvalid');
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-
-
-    // Check if file already exists
-    if (file_exists($target_file)) {
-      echo "Sorry, file already exists.";
+    // Allow certain file formats
+    if(empty($_FILES["fileToUpload"]["name"])) {
+      echo "<p class=\"roady-error-message\">A Twine html file was not selected. Please select a Twine html file to upload!</p>";
+      $uploadOk = 0;
+    }
+    // Allow certain file formats
+    if($uploadedFileType != "html") {
+      if($uploadOk !== 0) { echo "<p class=\"roady-error-message\">Only Twine html files can be uploaded! Please select a Twine html file to upload</p>"; }
       $uploadOk = 0;
     }
 
     // Check file size
     if (($_FILES["fileToUpload"]["size"] ?? 5000000) > 5000000) {
-      echo "Sorry, your file is too large.";
+      if($uploadOk !== 0) { echo "<p class=\"roady-error-message\">The file is too large!</p>"; }
       $uploadOk = 0;
     }
 
-    // Allow certain file formats
-    if($uploadedFileType != "html") {
-      echo "Sorry, only html files are allowed.";
+    // Check if file already exists
+    if (file_exists($target_file)) {
+      if($uploadOk !== 0) { echo "<p class=\"roady-error-message\">A Twine file with the same name was already uploaded. Please upload a Twine file with a unique name!</p>"; }
       $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-      echo "Sorry, your file was not uploaded.";
-    // if everything is ok, try to upload file
-    }
-
-    else {
+    if ($uploadOk !== 0) {
         if(!is_dir($target_dir)) {
             mkdir($target_dir);
         }
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-      } else {
-        echo "Sorry, there was an error uploading your file.";
-      }
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "<p class=\"roady-success-message\">The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.</p>";
+        } else {
+            echo "<p class=\"roady-error-message\">Sorry, there was an error uploading your file.</p>";
+        }
     }
 }
 ?>
